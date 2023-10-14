1e0a76c6bb270a62b8e4abf518cd648afc97b83d
RaveBlue <my-raveblue21@yahoo.com>
2023-10-13 22:50:31 -0600

    cart folder updated

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,16 +1,25 @@
 import { useState } from 'react';
 import Button from '../../ui/Button';
+import { useDispatch } from 'react-redux';
+import { updateName } from './userSlice';
+import { useNavigate } from 'react-router-dom';
 
 function CreateUser() {
   const [username, setUsername] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!username) return;
+    dispatch(updateName(username));
+    navigate('/menu');
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <p className="mb-10 text-sm text-stone-600 md:text-base">
+      <p className="mb-4 text-sm text-stone-600 md:text-base">
         ✨ Welcome! Please enter your name:
       </p>
 
@@ -19,7 +28,7 @@ function CreateUser() {
         placeholder="Your full name"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
-        className="w-72"
+        className="input mb-8 w-72"
       />
 
       {username !== '' && (
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,5 +1,9 @@
 import CreateUser from '../features/user/CreateUser';
+import { useSelector } from 'react-redux';
+import Button from './Button';
 function Home() {
+  const username = useSelector((state) => state.user.username);
+
   return (
     <div className="my-10 px-4 text-center sm:my-16">
       <h1
@@ -11,7 +15,13 @@ function Home() {
         <span className="text-blue-300">Taste Greatness in Every Slice</span>
       </h1>
 
-      <CreateUser />
+      {username === '' ? (
+        <CreateUser />
+      ) : (
+        <Button to="/menu" type="primary">
+          Continue ordering, {username}
+        </Button>
+      )}
     </div>
   );
 }
